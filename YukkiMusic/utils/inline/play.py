28e819606fb2832e0723ba622f80d6a5e17ffe99
itsadiazu <111038829+itsadiazu@users.noreply.github.com>
2022-08-15 11:15:41 +0530

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -31,7 +31,7 @@ selections = [
 ## After Edits with Timer Bar
 
 
-def stream_markup_timer(_, videoid, chat_id, played, current_time):
+def stream_markup_timer(_, videoid, chat_id, played, current_time, total time):
     bar = random.choice(selections)
     buttons = [
         [
